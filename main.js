c2746a897101c1cc29e69f2e5434d96380b936ae
Francesca-Barcella <pollon.fra@gmail>
2022-10-21 11:21:36 +0200

    Js - 01 - creazione array

--- main.js
+++ main.js
@@ -1,1 +1,76 @@
-alert('funziona1')
+/*******************************************************  
+TRACCIA
+Crea un array composto da 10 automobili.
+Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+Infine stampa separatamente i 3 array.
+*******************************************************/
+
+const automobili = [
+    // - 1
+    {
+        marca: 'ford',
+        modello: 'puma',
+        alimentazione: 'benzina'
+    },
+    // - 2
+    {
+        marca: 'ford',
+        modello: 'puma',
+        alimentazione: 'diesel'
+    },
+    // - 3
+    {
+        marca: 'lancia',
+        modello: 'stratos',
+        alimentazione: 'benzina'
+    },
+    // - 4
+    {
+        marca: 'wolkswagen',
+        modello: 'polo',
+        alimentazione: 'gpl'
+    },
+    // - 5
+    {
+        marca: 'wolkswagen',
+        modello: 'golf',
+        alimentazione: 'metano'
+    },
+    // - 6
+
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'elettrica'
+    },
+    // - 7
+
+    {
+        marca: 'tesla',
+        modello: 'model s',
+        alimentazione: 'elettrica'
+    },
+    // - 8
+
+    {
+        marca: 'renault',
+        modello: 'clio',
+        alimentazione: 'gpl'
+    },
+    // - 9
+
+    {
+        marca: 'lancia',
+        modello: 'ypsilon',
+        alimentazione: 'metano'
+    },
+    // - 10
+
+    {
+        marca: 'fiat',
+        modello: '500',
+        alimentazione: 'benzina'
+    },
+
+]
